1994a86d6bc6b0d12eeefd0247f614da4a8aa51f
Ramykha007 <76839588+Ramykha007@users.noreply.github.com>
2023-12-03 23:13:35 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+
+    <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px; " >
+
+      <div class="w3-center"  style=" background-image: url("backcover.png");"><br>
+      
+        <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Modal">&times;</span>
+        <img src="https://cdn-icons-png.flaticon.com/512/5309/5309792.png" alt="Avatar" style="width:50%" >
+      </div>
+
+<center>
+    <div style="
+      background-color: lightgrey;
+  width: 500px;
+  border: 5px solid yellow;
+  padding: 50px;
+  margin: 20px;
+  font-weight: bold;
+   text-align: left;
+   color:black;
+      ">
+⏱️ التحديث رقم 3.0.1 بتاريخ 3/12/2023 <br> 
+✔️ تم تحديث الخدمات الامنية الخاصة بالاسكربت <br> 
+✔️ تم اضافة اي بيهات جديدة تركية تساعد علي سرعة سحب وتنفيذ ال API<br>
+✔️ إصلاح بعض المشاكل في قائمة الكوبون<br>
+⚠️ جميع الحقوق محفوظة فقط لدي رامي خالد المحق الاول لمصدر الاسكربت من برفكت بانل <br> 
+📞 في حالة طلب التفاصيل اكثر تواصل معنا عبر الهاتف<br>
+</div></center>   
+
+
+      <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
+  
+        <span class="w3-right w3-padding w3-hide-small"><a href="https://api.whatsapp.com/send?phone=201126106073" style="color:black;">Call GM</a></span>
+      </div>
+
+    </div>
+
+
+
+</body>
+</html>
+
